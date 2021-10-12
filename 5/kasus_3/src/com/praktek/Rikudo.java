56f5440e6f0fa2dec99d191f3f9b0d12df0cce22
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2021-10-13 06:43:56 +0700

    kasus 3 selesai

---
+++ 5/kasus_3/src/com/praktek/Rikudo.java
@@ -0,0 +1,6 @@
+package com.praktek;
+
+public class Rikudo {
+	protected String Dojutsu = "Rinnegan";
+	protected String Senjutsu = "Sage Mode";
+}
---
+++ 5/kasus_3/src/com/praktek/Sasuke.java
@@ -0,0 +1,10 @@
+package com.praktek;
+
+public class Sasuke extends Itachi{
+	String Dojutsu = "Sharingan";
+	
+	void printDojutsu() {
+		super.printDojutsu();
+		System.out.println(this.Dojutsu);
+	}
+}
