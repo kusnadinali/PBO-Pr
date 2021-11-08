3a386981197fefa1ee5c4acf457ee433a45a3db1
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2021-11-09 01:16:21 +0700

    Lat 7.3 selesai

---
+++ 8/Lat 7.3/src/com/praktek/Commission.java
@@ -0,0 +1,28 @@
+package com.praktek;
+
+public class Commission extends Hourly{
+	private double totalSales;
+	private double commissionRate;
+	
+	public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate,double commissionRate) {
+		super(eName, eAddress, ePhone, socSecNumber, rate);
+		// TODO Auto-generated constructor stub
+		this.commissionRate = commissionRate;
+	}
+	public void addSales(double totalSales) {
+		this.totalSales = totalSales;
+	}
+	public double pay()
+	{
+		double payment = super.pay() + super.pay()*this.commissionRate;
+		return payment;
+	}
+	public String toString()
+	{
+	  String result = super.toString();
+
+	  result += "\nTotal Sales: " + totalSales;
+
+	  return result;
+	}
+}
