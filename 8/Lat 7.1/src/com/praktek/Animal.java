78205a428c9b0017957e346a5cebb41c44b332eb
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2021-11-09 00:20:13 +0700

    Lat 7.1 dan 7.2 sudah selesai

---
+++ 8/Lat 7.1/src/com/praktek/Animal.java
@@ -0,0 +1,7 @@
+package com.praktek;
+
+public class Animal {
+	public void sound() {
+		System.out.println("Animal is making a sound");
+	}
+}
---
+++ 8/Lat 7.1/src/com/praktek/Cat.java
@@ -0,0 +1,11 @@
+package com.praktek;
+
+public class Cat extends Animal {
+	public void sound() {
+		System.out.println("Meow");
+	}
+	public static void main(String[] args) {
+		Animal obj=new Cat();
+		obj.sound();
+	}
+}
